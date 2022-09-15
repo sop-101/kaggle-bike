ca2c97f386922480250cc176f4632c08cf975e8f
sop-101 <69847872+sop-101@users.noreply.github.com>
2022-09-15 16:51:35 +0100

    split-datetime

--- bike-sharing.py
+++ bike-sharing.py
@@ -11,3 +11,12 @@ train = pd.read_csv('bike-sharing-demand/train.csv')
 test = pd.read_csv('bike-sharing-demand/test.csv')
 
 #print(train.head()) check to see if csv file has been read properly
+df = pd.DataFrame(train)
+
+#print(train.dtypes())
+#season, holiday, workingday, weather = categorical data
+
+#split datetime
+df['Year'] = pd.DatetimeIndex(df['datetime']).year
+df['Month'] = pd.DatetimeIndex(df['datetime']).month
+df['Hour'] = pd.DatetimeIndex(df['datetime']).hour
